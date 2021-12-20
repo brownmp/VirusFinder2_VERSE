70beec78a28098835587de27e77ed0083c4d37d1
brownmp <mbrown@broadinstitute.org>
2021-12-20 14:48:26 -0500

    update

--- write_configuration_file.py
+++ write_configuration_file.py
@@ -61,9 +61,10 @@ def main():
 
     virus_database = os.path.join(cwd, args.virus_database)
     blastn_index_virus = os.path.join(cwd, args.blastn_index_virus)
-    bowtie_index_human = args.bowtie_index_human
+    # bowtie_index_human = args.bowtie_index_human
     # blastn_index_human = args.blastn_index_human
-    blastn_index_human = os.path.join(cwd, args.blastn_index_human)
+    bowtie_index_human = os.path.join(cwd, args.blastn_index_human, "GRCh38.genome")
+    blastn_index_human = os.path.join(cwd, args.blastn_index_human, "GRCh38.genome")
 
 
     detection_mode = args.detection_mode
